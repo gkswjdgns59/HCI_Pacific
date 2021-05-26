b078e818d597115bc0d06e242a148a07a91eccbb
Han Jeonghoon <gkswjdgns59@kaist.ac.kr>
2021-05-26 13:27:10 +0900

    aa

--- react-app/src/components/SelectGuestDialog.js
+++ react-app/src/components/SelectGuestDialog.js
@@ -2,9 +2,114 @@ import React from 'react';
 import { AccordionSummary,AccordionDetails,Accordion,InputAdornment, ListItemAvatar, Chip, List, ListItem, ListItemText, ListItemSecondaryAction, Checkbox, Divider, Button, TextField, DialogContent, Dialog, DialogActions, DialogContentText, DialogTitle, Typography, Avatar} from '@material-ui/core';
 import { makeStyles, withStyles } from '@material-ui/core/styles';
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
-import {ReactComponent as Blob1} from '../blobs/blob-haikei (1).svg';
 import SearchIcon from '@material-ui/icons/Search'
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import {ReactComponent as Blob1} from '../blobs/blob-haikei (1).svg';
+import {ReactComponent as Blob2} from '../blobs/blob-haikei (2).svg';
+import {ReactComponent as Blob3} from '../blobs/blob-haikei (3).svg';
+import {ReactComponent as Blob4} from '../blobs/blob-haikei (4).svg';
+import {ReactComponent as Blob5} from '../blobs/blob-haikei (5).svg';
+import {ReactComponent as Blob6} from '../blobs/blob-haikei (6).svg';
+import {ReactComponent as Blob7} from '../blobs/blob-haikei (7).svg';
+import {ReactComponent as Blob8} from '../blobs/blob-haikei (8).svg';
+import {ReactComponent as Blob9} from '../blobs/blob-haikei (9).svg';
+import {ReactComponent as Blob10} from '../blobs/blob-haikei (10).svg';
+import {ReactComponent as Blob11} from '../blobs/blob-haikei (11).svg';
+import {ReactComponent as Blob12} from '../blobs/blob-haikei (12).svg';
+import {ReactComponent as Blob13} from '../blobs/blob-haikei (13).svg';
+import {ReactComponent as Blob14} from '../blobs/blob-haikei (14).svg';
+import {ReactComponent as Blob15} from '../blobs/blob-haikei (15).svg';
+import {ReactComponent as Blob16} from '../blobs/blob-haikei (16).svg';
+import {ReactComponent as Blob17} from '../blobs/blob-haikei (17).svg';
+import {ReactComponent as Blob18} from '../blobs/blob-haikei (18).svg';
+import {ReactComponent as Blob19} from '../blobs/blob-haikei (19).svg';
+import {ReactComponent as Blob20} from '../blobs/blob-haikei (20).svg';
+
+const makeblob=(num,fill)=>{
+    
+    switch(num) {
+        case 1:
+            return (
+                <Blob1 fill={fill} width='15%' height='15%'></Blob1>
+            )
+        case 2:
+            return (
+                <Blob2 fill={fill} width='15%' height='15%'></Blob2>
+            )
+        case 3:
+            return (
+                <Blob3 fill={fill} width='15%' height='15%'></Blob3>
+            )
+        case 4:
+            return (
+                <Blob4 fill={fill} width='15%' height='15%'/>
+            )
+        case 5:
+            return (
+                <Blob5 fill={fill} width='15%' height='15%'/>
+            )
+        case 6:
+            return (
+                <Blob6 fill={fill} width='15%' height='15%'/>
+            )
+        case 7:
+            return (
+                <Blob7 fill={fill} width='15%' height='15%'/>
+            )
+        case 8:
+            return (
+                <Blob8 fill={fill} width='15%' height='15%'/>
+            )
+        case 9:
+            return (
+                <Blob9 fill={fill} width='15%' height='15%'/>
+            )
+        case 10:
+            return (
+                <Blob10 fill={fill} width='15%' height='15%'/>
+            )
+        case 11:
+            return (
+                <Blob11 fill={fill} width='15%' height='15%'/>
+            )
+        case 12:
+            return (
+                <Blob12 fill={fill} width='15%' height='15%'/>
+            )
+        case 13:
+            return (
+                <Blob13 fill={fill} width='15%' height='15%'/>
+            )
+        case 14:
+            return (
+                <Blob14 fill={fill} width='15%' height='15%'/>
+            )
+        case 15:
+            return (
+                <Blob15 fill={fill} width='15%' height='15%'/>
+            )
+        case 16:
+            return (
+                <Blob16 fill={fill} width='15%' height='15%'/>
+            )
+        case 17:
+            return (
+                <Blob17 fill={fill} width='15%' height='15%'/>
+            )
+        case 18:
+            return (
+                <Blob18 fill={fill} width='15%' height='15%'/>
+            )
+        case 19:
+            return (
+                <Blob19 fill={fill} width='15%' height='15%'/>
+            )
+        case 20:
+            return (
+                <Blob20 fill={fill} width='15%' height='15%'/>
+            )
+    }
+}
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -46,10 +151,13 @@ const useStyles = makeStyles((theme) => ({
 
 
 export default function SelectGuestDialog() {
-    //list only for demo
-    const loadedGuest = ['Sehoon Lim', 'Byungwoo Choi', 'Jeonghoon Han', 'Jaeryung Chung', 'Woojung Jun', 'Gahyeon Lee', 'Jayun Ku', 'Soeun Kim', 'Seongjun Hwang', 'Seokju Lee'];
-    //
-    
+    let inputObj = {'Sehoon Lim':{"blob_num": 2, "blob_fill":1}, 'Byungwoo Choi':{"blob_num": 1, "blob_fill":5}, 'Jaeryung Chung':{"blob_num": 5, "blob_fill":3}, 'Jeonghoon Han':{"blob_num": 7, "blob_fill":2},'Woojung Jun':{"blob_num": 6, "blob_fill":4}} //아까 니가 말한 input
+
+    let loadedGuest = [];
+    for (let person in inputObj){
+        loadedGuest.push(person)
+    }
+    const color_Set=['#e6e6fa','#faece6','#e6f3fa','#e7fae6','#faf8e6']
     const [open, setOpen] = React.useState(false);
     const [checked, setChecked] = React.useState(()=>{
         const guestObject = {};
@@ -82,6 +190,15 @@ export default function SelectGuestDialog() {
             return guestObject;
         })
     };
+
+    const handleCloseAdd = () => {
+        const resultData={};
+        for (let i=0; i<chipData.length; i++){
+            resultData[chipData[i].label]=inputObj[chipData[i].label];
+        }
+        console.log(resultData); //output consolelog 대신 callback 함수 넣어서 부모 component로 올리면 될 듯
+        handleClose();
+    }
 
     const onListChange = (value) => {
         setListData(value);
@@ -147,7 +264,6 @@ export default function SelectGuestDialog() {
     
     const handleAccordion = () => {
         if (newGuest[0]!==null){
-            //firebase upload
             let copyList = [...listData];
             let copyData = [...chipData];
             let copyChecked = JSON.parse(JSON.stringify(checked));
@@ -157,6 +273,8 @@ export default function SelectGuestDialog() {
                 key: newGuest[0],
                 label: newGuest[0]
             })
+            inputObj[newGuest[0]]={'blob_num':Math.floor(Math.random(0)*5+1), 'blob_fill':Math.floor(Math.random(0)*5+1)};
+            console.log(inputObj)
             setChipData(copyData);
             setChecked(copyChecked);
             onListChange(copyList);
@@ -186,7 +304,12 @@ export default function SelectGuestDialog() {
     }
 
     const handleExpanded = () => {
-        setExpaned(true);
+        if (expanded){
+            setExpaned(false);
+        }else{
+            setExpaned(true);
+        }
+        
     }
 
     const theme = createMuiTheme({
@@ -244,7 +367,8 @@ export default function SelectGuestDialog() {
                 <List className={classes.root}>
                     {listData.map((guest)=>(
                         <ListItem button onClick={()=>handleListItemClick(guest)} key={guest}>
-                            <Blob1 fill='#E6E6FA' className={classes.Blob}></Blob1>
+                            {/* <Blob1 fill='#E6E6FA' className={classes.Blob}></Blob1> */}
+                            {makeblob(inputObj[guest].blob_num, color_Set[inputObj[guest].blob_fill-1])}
                             <ListItemText primary={guest} />
                             <ListItemSecondaryAction>
                                 <Checkbox
@@ -302,7 +426,7 @@ export default function SelectGuestDialog() {
                 <Button onClick={handleClose}>
                     Cancel
                 </Button>
-                <Button onClick={handleClose} variant="outlined" color="primary">
+                <Button onClick={handleCloseAdd} variant="outlined" color="primary">
                     Add
                 </Button>
                 </DialogActions>
